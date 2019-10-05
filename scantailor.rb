391c57ea8e34520c60ad2fef1081bfc6476eb33e
Daeyong Jung <bearrundr@hotmail.com>
2019-10-06 00:09:02 +0900

    change url information

--- scantailor.rb
+++ scantailor.rb
@@ -1,11 +1,11 @@
 class Scantailor < Formula
-  desc "Scantailor (Advanced) : bookscan images processor"
-  homepage "https://github.com/4lex4/scantailor-advanced"
+  desc "Scantailor (custom) : bookscan images processor"
+  homepage "https://github.com/bearrundr/scantailor-custom"
   
-  url "https://github.com/4lex4/scantailor-advanced/archive/v1.0.16.tar.gz"
+  url "https://github.com/bearrundr/scantailor-custom/archive/v1.0.0.tar.gz"
   sha256 "84629d2edba4c36c62bdb75eedb145262b894d950bcb95cec0dab43e21bdb909"
 
-  head "https://github.com/4lex4/scantailor-advanced.git"
+  head "https://github.com/bearrundr/scantailor-custom.git"
   
   depends_on "boost"
   depends_on "qt5"
